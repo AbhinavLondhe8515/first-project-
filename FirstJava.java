2644111bfdce70544fbef9e029d93c06f9c8d3cc
AbhinavLondhe8515 <59164413+AbhinavLondhe8515@users.noreply.github.com>
2022-12-10 00:10:58 +0530

    Update FirstJava.java

--- FirstJava.java
+++ FirstJava.java
@@ -1,8 +1,8 @@
  public class FirstJava
     {
-      public sttaic void main(String[] args)
+      public static void main(String[] args)
        {
-         System.out.pritnln("Hello! Welcome to Jenkins!") ;
+         System.out.println("Hello! Welcome to Jenkins!") ;
        }
     }
 
